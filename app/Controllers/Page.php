0339064e45c75c3c54e0429575e994ada51f2641
olizyusuf <olizyusuf22@gmail.com>
2020-10-14 15:51:57 +0700

    menambah routes dan membuah file  page disi test dengan function

---
+++ app/Controllers/Page.php
@@ -0,0 +1,21 @@
+<?php namespace App\Controllers;
+
+class Page extends BaseController
+{
+    public function about()
+    {
+        echo "Halaman About";
+    }
+
+    public function contact()
+    {
+        echo "Halaman Contact";
+    }
+
+    public function faqs()
+    {
+        echo "Halaman FAQ";
+    }
+}
+
+?>
